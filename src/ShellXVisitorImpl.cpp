3bfcfaf662ce3129d3c69edca48916bb7ede98a0
Carlos Castilla <63982489+thecurz@users.noreply.github.com>
2023-11-22 07:30:52 -0500

    ls

--- src/ShellXVisitorImpl.cpp
+++ src/ShellXVisitorImpl.cpp
@@ -1,17 +1,31 @@
 #include "../headers/ShellXVisitorImpl.h"
-
-std::any ShellXVisitorImpl::visitProgram(ShellXParser::ProgramContext* ctx) {
-    for (auto context : ctx->command()) {
-        auto LsCommandContext = dynamic_cast<ShellXParser::LsCommandContext*>(context);
-        if (LsCommandContext) {
-            visitLsCommand(LsCommandContext);
-            continue;
-        }
+#include <cstdlib>
+#include <dirent.h>
+std::any ShellXVisitorImpl::visitProgram(ShellXParser::ProgramContext *ctx) {
+  for (auto context : ctx->command()) {
+    auto LsCommandContext =
+        dynamic_cast<ShellXParser::LsCommandContext *>(context);
+    if (LsCommandContext) {
+      visitLsCommand(LsCommandContext);
+      continue;
     }
-    return visitChildren(ctx);
+  }
+  return visitChildren(ctx);
 }
 
-std::any ShellXVisitorImpl::visitLsCommand(ShellXParser::LsCommandContext* ctx) {
-    std::cout << "visitLsCommand" << std::endl;
-    return visitChildren(ctx);
+std::any
+ShellXVisitorImpl::visitLsCommand(ShellXParser::LsCommandContext *ctx) {
+  // TODO: Implement and test ls behavior
+  std::cout << "DEBUG: ls command" << std::endl;
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir("./")) != NULL) {
+    while ((ent = readdir(dir)) != NULL) {
+      printf("%s\n", ent->d_name);
+    }
+    closedir(dir);
+  } else {
+    perror("");
+  }
+  return visitChildren(ctx);
 }
