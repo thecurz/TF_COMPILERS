d1cf5a7f4eb4c11cfc8d349e38becb666f412562
Carlos Castilla <63982489+thecurz@users.noreply.github.com>
2023-11-25 10:41:31 -0500

    interactivo

--- main.cpp
+++ main.cpp
@@ -7,21 +7,49 @@
 #include "tree/ParseTree.h"
 
 int32_t main(int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cerr << "Por favor, ingrese un archivo como input." << std::endl;
-    return 1;
-  }
+  // if (argc < 2) {
+  //   std::cerr << "Por favor, ingrese un archivo como input." << std::endl;
+  //   return 1;
+  // }
 
   std::ifstream inputFile(argv[1]);
-  if (!inputFile) {
-    std::cerr << "Error al leer el archivo: " << argv[1] << std::endl;
-    return 1;
+  bool noinput = false;
+  if (argc >= 2) {
+    if (!inputFile) {
+      std::cerr << "Error al leer el archivo: " << argv[1] << std::endl;
+      // continue reading
+    }
+  } else {
+    noinput = true;
   }
 
   std::string line;
   std::stringstream fileContent;
-  while (std::getline(inputFile, line)) {
-    fileContent << line << "\n";
+  if (noinput) {
+    std::cout << "$ShellX>";
+  }
+
+  while (std::getline((inputFile.is_open() ? inputFile : std::cin), line)) {
+    // while (std::getline(inputFile, line)) {
+    std::stringstream currentLine;
+    currentLine << line << "\n";
+    fileContent << currentLine.str();
+    if (noinput) {
+      antlr4::ANTLRInputStream input(currentLine.str());
+
+      ShellXLexer lexer(&input);
+      antlr4::CommonTokenStream tokens(&lexer);
+      ShellXParser parser(&tokens);
+
+      antlr4::tree::ParseTree *tree = parser.program();
+      ShellXVisitorImpl visitor;
+      // std::cout << "tree:\n" << tree->toStringTree(&parser) << std::endl;
+      visitor.visit(tree);
+      std::cout << "\n$ShellX>";
+    }
+  }
+  if (noinput) {
+    return 0;
   }
   std::string wholeFile = fileContent.str();
   inputFile.close();
